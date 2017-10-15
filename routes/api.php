44fcfaf1c3453a9a2b75a4e3294ff1abe4c1561b
ANdy Wu <admin@andy-web-dev.com>
2017-10-15 13:51:47 -0400

    move to api v1

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// get list of animals
-Route::get('shelter_animals/{filter?}','ShelterAnimalController@index')->where('filter', '(.*)');
-// shuffle random page
-Route::get('shelter_animals/random','ShelterAnimalController@getRandomPage');
-// get single page
-Route::get('shelter_animals/{page}/{filter?}','ShelterAnimalController@getPage')
-->where(['page' => '[0-9]+', 'filter' => '(.*)']);
+Route::group(['prefix' => 'v1'], function()
+{
+    // get list of animals
+    Route::get('shelter_animals/{filter?}','Api\v1\ShelterAnimalController@index')->where('filter', '(.*)');
+    // shuffle random page
+    Route::get('shelter_animals/random','Api\v1\ShelterAnimalController@getRandomPage');
+    // get single page
+    Route::get('shelter_animals/{page}/{filter?}','Api\v1\ShelterAnimalController@getPage')
+    ->where(['page' => '[0-9]+', 'filter' => '(.*)']);
+});
