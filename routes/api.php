af5f30375ca18d03b74d7cbc9cce204b7a019553
ANdy Wu <admin@andy-web-dev.com>
2017-10-16 00:58:47 -0400

    re-order api routes

--- routes/api.php
+++ routes/api.php
@@ -19,11 +19,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::group(['prefix' => 'v1'], function()
 {
-    // get list of animals
-    Route::get('shelter_animals/{filter?}','Api\v1\ShelterAnimalController@index')->where('filter', '(.*)');
     // shuffle random page
     Route::get('shelter_animals/random','Api\v1\ShelterAnimalController@getRandomPage');
     // get single page
     Route::get('shelter_animals/{page}/{filter?}','Api\v1\ShelterAnimalController@getPage')
     ->where(['page' => '[0-9]+', 'filter' => '(.*)']);
+    // get list of animals
+    Route::get('shelter_animals/{filter?}','Api\v1\ShelterAnimalController@index')->where('filter', '(.*)');
 });
