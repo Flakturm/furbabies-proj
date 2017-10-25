90377606e1d4a8c55689bb05cc589c4185d18708
ANdy Wu <admin@andy-web-dev.com>
2017-10-25 11:50:24 -0400

    [Helper] 404 helper

--- app/helpers.php
+++ app/helpers.php
@@ -3,7 +3,8 @@
 /**
  * Global helpers file with misc functions.
  */
-if (! function_exists('app_name')) {
+if (! function_exists('app_name'))
+{
     /**
      * Helper to grab the application name.
      *
@@ -12,5 +13,25 @@ if (! function_exists('app_name')) {
     function app_name()
     {
         return env('APP_NAME');
+    }
+}
+
+if (! function_exists('is_404'))
+{
+    /**
+     * Helper to check if the url is broken or not.
+     *
+     * @return boolean
+     */
+    function is_404( $url )
+    {
+        if ( empty( $url ) )
+        {
+            return true;
+        }
+
+        $headers = get_headers( $url );
+
+        return strpos( $headers[0], '404' ) ? true : false;
     }
 }
