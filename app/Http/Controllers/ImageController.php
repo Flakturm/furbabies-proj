1b690d50afc767855a1a3ef5551e7c7343552c8c
ANdy Wu <admin@andy-web-dev.com>
2017-12-05 21:36:37 +0800

    best practices

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -8,16 +8,10 @@ use Cache;
 
 class ImageController extends Controller
 {
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request)
+    public function update()
     {
-        $shelterAnimal = ShelterAnimal::find( $request->id );
-        if ( $shelterAnimal == null )
+        $shelterAnimal = ShelterAnimal::find( request('id') );
+        if ( is_null( $shelterAnimal ) )
         {
             return response()->json('Something is wrong with this id. ID#' . $request->id);
         }
@@ -26,7 +20,10 @@ class ImageController extends Controller
             $shelterAnimal->album_file = asset('images/nophoto.jpg');
             $shelterAnimal->save();
 
-            Cache::forget('shelteranimals_page_' . $request->page);
+            if ( $request->page )
+            {
+                Cache::forget('shelteranimals_page_' . $request->page);
+            }
 
             return response()->json(['message' => $request->id . ' album_file wiped.']);
         }
